dbf12cfca130fd37f825fe7c3c1b867561878872
BUBA <buba86@ro.ru>
2023-07-10 22:14:36 +0500

    initial commit1

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,7 @@ public class Main {
         if (replenishment > 1000) {
             System.out.println("Итоговый счёт" + (startingScore + replenishment + bonus));
             System.out.println("Количество бонусных рублей" + bonus);
-        }
-        else {
+        } else {
             System.out.println("Итоговый счёт" + (startingScore + replenishment));
             System.out.println("Количество бонусных рублей" + 0);
         }
